77d7676b6ecaffb01ba2a2e0c2f04c14a790e48d
Malaka <ithayg@gmail.com>
2025-01-24 18:02:59 +0200

    changed tables to be static const members

--- src/chip-8.cpp
+++ src/chip-8.cpp
@@ -1,14 +1,16 @@
 
+#include <cstdint>
 #include <fstream>
-#include <iterator>
+#include <functional>
 #include <iostream>
+#include <unordered_map>
 
 #include "chip-8.hpp"
 
 namespace Emulator
 {
 
-CHIP_8::CHIP_8() : m_opcodes_table(InitOpcodesTable())
+CHIP_8::CHIP_8()
 {   
     LoadFonts(m_memory);
 }
@@ -37,13 +39,13 @@ void CHIP_8::EmulateCycle() {
     uint16_t first_4_bits = opcode & 0xF000;
     if (0x0000 == first_4_bits) {
         uint16_t last_4_bits = opcode & 0x000F;
-        if (m_opcodes_table.end() != m_opcodes_table.find(last_4_bits)) {
-            m_opcodes_table[last_4_bits](this);
+        if (s_opcode_tabale.end() != s_opcode_tabale.find(last_4_bits)) {
+            s_opcode_tabale.at(last_4_bits)(this);
         } else {
             std::cout << "unknown opcode [0x0000]: " << opcode << '\n';
         } 
-    } else if (m_opcodes_table.end() != m_opcodes_table.find(first_4_bits)) {
-        m_opcodes_table[first_4_bits](this);
+    } else if (s_opcode_tabale.end() != s_opcode_tabale.find(first_4_bits)) {
+        s_opcode_tabale.at(first_4_bits)(this);
     } else {
         std::cout << "unknown opcode: " << opcode << '\n';
     }
@@ -62,17 +64,8 @@ void CHIP_8::EmulateCycle() {
 
 inline void CHIP_8::LoadFonts(std::array<unsigned char, MEMORY_SIZE>& memory) {
     for (std::size_t i = 0; i < NUM_OF_FONTS; ++i) {
-        memory[i] = s_font_set[i];
+        memory[i] = s_fonts_set[i];
     }
-}
-
-inline std::unordered_map<uint16_t, std::function<void(CHIP_8*)>> CHIP_8::InitOpcodesTable() {
-    std::unordered_map<uint16_t, std::function<void(CHIP_8*)>> opcodes_table = 
-    {
-        {0x200, &Op0x200},
-    }; 
-
-    return opcodes_table;
 }
 
 void CHIP_8::Op0x200(CHIP_8 *chip) {
@@ -82,5 +75,30 @@ void CHIP_8::Op0x200(CHIP_8 *chip) {
     chip->m_stack.MoveSP(1);
     chip->m_pc = chip->m_opcode & ADDR_NNN;
 }
+
+const std::unordered_map<uint16_t, std::function<void(CHIP_8*)>>  CHIP_8::s_opcode_tabale =
+{
+    {0x200, &Op0x200}
+};
+
+const std::array<uint8_t, CHIP_8::NUM_OF_FONTS> CHIP_8::s_fonts_set =
+{
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
 
 } // Emulation
